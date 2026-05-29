4353d254ddf4c0e09dadbff4c14df06353058867
RewelliOliveira <rewelli@alu.ufc.br>
2026-05-29 11:09:41 -0300

    config: database connection

--- app/database/db.py
+++ app/database/db.py
@@ -1,13 +1,13 @@
-import os 
-from dotenv import load_dotenv
+import os
+from dotenv import find_dotenv, load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-load_dotenv()
+load_dotenv(find_dotenv())
 
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./aircraft.db")
 connect_args = {"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
-engine = create_engine(DATABASE_URL, connet_args=connect_args)
+engine = create_engine(DATABASE_URL, connect_args=connect_args)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
